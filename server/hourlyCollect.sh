ac60b11b5ebb21e3fc274cf25ebd7d00d1d1d364
leozhang2018 <leozhang2018@gmail.com>
2016-07-03 17:47:19 +0800

    Add hourlycollect

--- server/hourlyCollect.sh
+++ server/hourlyCollect.sh
@@ -3,7 +3,7 @@
 
 #捕获间隔
 
-sleepTime="100"
+sleepTime="3600"
 
 #当前时间
 currentTime=$(date +%s)
@@ -14,7 +14,7 @@ echo $(date +%s)
 echo $filename
 
 # 写入起始时间
-date +%s | awk '{print "start_timestamp:" $1 }' > ./Timestamp/$filename
+date +%s | awk '{print "start_timestamp:" $1 }' > ./Timestamp/hourly/$filename
 
 tcpdump -i p3p1 tcp[20:2]=0x4745 or tcp[20:2]=0x504f -w tcp.cap -s 512 2>&1 &
 
@@ -27,11 +27,12 @@ echo "Stringfy Ok"
 
 #strings tcp.cap | grep -E "GET /|POST /|Host:" | grep --no-group-separator -B 1 "Host:" | grep --no-group-separator -A 1 -E "GET /|POST /" | awk '{url=$2;getline;host=$2;printf ("%s\n",host""url)}' > /tmp/url.txt
 
-date +%s | awk '{print "end_timestamp:" $1 }' >> ./Timestamp/$(date +%s)
+# 写入结束时间
+date +%s | awk '{print "end_timestamp:" $1 }' >> ./Timestamp/hourly/$(date +%s)
 
 grep -v -i -E "^$" url_collect | awk -F '/' '{print $1}' | sort | uniq -c | sort -nr >> ./Timestamp/$(date +%s)
 
 echo "Decode OK"
-cat Timestamp/$(date +%s)
+cat Timestamp/hourly/$(date +%s)
 
 kill `ps aux | grep tcpdump | grep -v grep | awk '{print $2}'`
