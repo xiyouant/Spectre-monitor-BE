efc2bf3703617cebe781e2caf50a12ed5cbdd188
leozhang2018 <leozhang2018@gmail.com>
2016-07-03 16:15:43 +0800

    Modified folder structure

--- server/tcpdump.sh
+++ server/tcpdump.sh
@@ -21,7 +21,7 @@ tcpdump -i p3p1 tcp[20:2]=0x4745 or tcp[20:2]=0x504f -w tcp.cap -s 512 2>&1 &
 # 捕获间隔
 sleep $sleepTime
 
-strings tcp.cap | grep -E "Host:"|awk -F ' ' '{print $2 }' >>  url.txt
+strings tcp.cap | grep -E "Host:"|awk -F ' ' '{print $2 }' >>  url_collect
 
 echo "Stringfy Ok"
 
