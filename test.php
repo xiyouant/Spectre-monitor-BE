cd971dd7c6513128b88be771244ff7fbad01f29a
leozhang2018 <leozhang2018@gmail.com>
2016-07-03 15:53:43 +0800

    controled by manual

--- test.php
+++ test.php
@@ -1,28 +1,35 @@
 <?php
 // 将一个文件读入数组。
 //读取时间戳文件为一维数组
-$urls = file('Timestamp/1467523890');
+$urlsArray = file('Timestamp/1467530662');
 
 //时间戳一维数组长度
-$timeStampArrayLength = count($urls);
+$timeStampArrayLength = count($urlsArray);
 print_r('<pre>');
 
 // 获取分割后的开始时间
-$startTimestamp=explode(':',$urls[0])[1];
+$startTimestamp=explode(':',$urlsArray[0])[1];
 
-//结束时间
-$endTimestamp=explode(':',$urls[1])[1];
+// 结束时间
+$endTimestamp=explode(':',$urlsArray[1])[1];
 
 echo "start : $startTimestamp";
 echo "end : $endTimestamp";
 
-for ($i = 2; $i < count($urls); $i++ )
+for ($i = 2; $i < count($urlsArray); $i++ )
 {
-  $i_length = count(explode(' ',$urls[$i]));
-  print_r("num:" .explode(' ',$urls[$i])[$i_length-2]);
+  // 依次将一维数组 [2 analy.qq.com] 按照 " "分割获得[2][analy.qq.com]
+  $i_length = count(explode(' ',$urlsArray[$i]));
+  $urlCount=explode(' ',$urlsArray[$i])[$i_length-2];
+  $url=explode(' ',$urlsArray[$i])[$i_length-1];
+
+  print_r("num: $urlCount");
   print_r('<pre>');
-  print_r("url:".explode(' ',$urls[$i])[$i_length-1]);
+  print_r("url: $url");
   print_r('<pre>');
+
+
+
 }
 
 ?>
