6f7163baa99d73dae847ef6a3462f4130ff844aa
leozhang2018 <leozhang2018@gmail.com>
2016-07-03 21:04:52 +0800

    Add set env

--- server/hourlyCollect.sh
+++ server/hourlyCollect.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+#设置环境变量
+export spectre="/home/wwwroot/default/server/"
+ 
 # Start capturing
 
 #捕获间隔
@@ -14,25 +18,25 @@ echo $(date +%s)
 echo $filename
 
 # 写入起始时间
-date +%s | awk '{print "start_timestamp:" $1 }' > ./Timestamp/hourly/$filename
+date +%s | awk '{print "start_timestamp:" $1 }' > $spectre/Timestamp/hourly/$filename
 
-tcpdump -i p3p1 tcp[20:2]=0x4745 or tcp[20:2]=0x504f -w ./Capture_Package/hourly/tcp.cap -s 512 2>&1 &
+tcpdump -i p3p1 tcp[20:2]=0x4745 or tcp[20:2]=0x504f -w $spectre/Capture_Package/hourly/tcp.cap -s 512 2>&1 &
 
 # 捕获间隔
 sleep $sleepTime
 
-strings ./Capture_Package/hourly/tcp.cap | grep -E "Host:"|awk -F ' ' '{print $2 }' >  hourly_url_collect
+strings $spectre/Capture_Package/hourly/tcp.cap | grep -E "Host:"|awk -F ' ' '{print $2 }' >  $spectre/hourly_url_collect
 
 echo "Stringfy Ok"
 
 #strings tcp.cap | grep -E "GET /|POST /|Host:" | grep --no-group-separator -B 1 "Host:" | grep --no-group-separator -A 1 -E "GET /|POST /" | awk '{url=$2;getline;host=$2;printf ("%s\n",host""url)}' > /tmp/url.txt
 
 # 写入结束时间
-date +%s | awk '{print "end_timestamp:" $1 }' >> ./Timestamp/hourly/$(date +%s)
+date +%s | awk '{print "end_timestamp:" $1 }' >> $spectre/Timestamp/hourly/$(date +%s)
 
-grep -v -i -E "^$"  hourly_url_collect | awk -F '/' '{print $1}' | sort | uniq -c | sort -nr >> ./Timestamp/hourly/$(date +%s)
+grep -v -i -E "^$"  $spectre/hourly_url_collect | awk -F '/' '{print $1}' | sort | uniq -c | sort -nr >> $spectre/Timestamp/hourly/$(date +%s)
 
 echo "Decode OK"
-cat Timestamp/hourly/$(date +%s)
+cat $spectre/Timestamp/hourly/$(date +%s)
 
 kill `ps aux | grep tcpdump | grep -v grep | awk '{print $2}'`
