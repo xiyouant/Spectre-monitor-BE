e4f402b6173d845354924f22f3c46ebf4e3d29a5
leozhang2018 <leozhang2018@gmail.com>
2016-08-14 18:07:50 +0800

    change api access method

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -4,52 +4,93 @@
 // domain_meta_query()->域名查询并处理
 // traffic_meta_query()->流量查询并处理
 // interface_meta_query()->网络接口查询并处理
-// serviceStatus()->Spectre 状态查询并处理
+// getServiceStatus()->Spectre 状态查询并处理
 
 # 封装接口 API 以及返回格式:
-/* 
-domain()
+/*
+/api/query?query=domain
 -->说明:单位时间内 http domain 访问 top10
 -->请求方式: get
 -->返回格式:对象数组
 
-traffic()
+/api/query?query=traffic
 -->说明:单位时间内网络接口流量情况 (默认满额为 top 10)
 -->请求方式: get
 -->返回格式:对象数组
 
-queryServiceStatus()
+/api/query?query=serviceStatus
 -->说明:Spectre 运行状态查询
 -->请求方式: get
 -->返回格式: json
 {
-  "redir": false,
-  "tunnel": false,
-  "memInfo": {
-    "available": 8279168,
-    "free": 4971220,
-    "total": 12253692
-  }
+"redir": false,
+"tunnel": false,
+"memInfo": {
+"available": 8279168,
+"free": 4971220,
+"total": 12253692
+}
 }
 
-serviceReload() 
+serviceReload()
 -->说明:服务(ss-tunnel||ss-redir)重新启动接口
 -->请求方式: post
 -->post 请求参数: restartService=重新启动的服务名称
 -->返回格式: json
 成功重启
 {
-  "restartService": "ss-redir",
-  "status": 0
+"restartService": "ss-redir",
+"status": 1
 }
 重启失败
 {
-  "restartService": "ss-redir",
-  "status": 1
+"restartService": "ss-redir",
+"status": 0
 }
 */
 
 class Api extends CI_Controller {
+    
+    /************************ utils function ********************************************/
+    //输出 json
+    private function jsonOutput($data){
+        $this->output
+        ->set_status_header(200)
+        ->set_header('Access-Control-Allow-Origin: *')
+        ->set_header('Cache-Control: no-store, no-cache, must-revalidate')
+        ->set_header('Pragma: no-cache')
+        ->set_content_type('application/json', 'utf-8')
+        ->set_output(json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES))
+        ->_display();
+        exit;
+    }
+    
+    
+    //处理 Post 和 Get 请求函数
+    //返回参数与 ip 构成的关联数组
+    //接受所有参数并经过 XSS 过滤 params= TRUE
+    private function resolveRequest(){
+        if ($this->input->method() == "get") {
+            $params = $this->input->get(NULL, TRUE);
+            $method = array('method' => 'get');
+        }
+        elseif ($this->input->method() == "post") {
+            $params = $this->input->post(NULL, TRUE);
+            $method = array('method' => 'post');
+        }
+        else{
+            return 0;
+        }
+        $ip=array('ip' => $this->input->ip_address());
+        return array_merge_recursive($method,$params,$ip);
+    }
+    
+    
+    
+    /************************ DB and file process function ********************************************/
+    
+    
+    
     private function domain_meta_query(){
         $query = $this->db->query('SELECT id,url,count,startTimestamp,endTimestamp FROM metadata');
         // 定义查询结果数组
@@ -78,8 +119,7 @@ class Api extends CI_Controller {
     }
     
     
-    private function interface_meta_query()
-    {
+    private function interface_meta_query(){
         $query = $this->db->query('SELECT interface FROM `interface`');
         // 定义查询结果数组
         $queryArray = $query->result_array();
@@ -97,8 +137,7 @@ class Api extends CI_Controller {
     }
     
     
-    public function traffic_meta_query()
-    {
+    private function traffic_meta_query(){
         //调用 interface_meta_query 查询结果
         $interfacesArray = $this->interface_meta_query();
         for ($i=0; $i < count($interfacesArray); $i++) {
@@ -108,14 +147,12 @@ class Api extends CI_Controller {
             $upload = [];
             // 定义时间戳数组
             $timeStamp = [];
-            
             $receiveArray = $this->db->query("SELECT trafficUsage FROM `traffic` WHERE interface='{$interfacesArray[$i]}' AND method ='receive' ORDER BY id DESC LIMIT 10");
             $transmitArray = $this->db->query("SELECT trafficUsage FROM `traffic` WHERE interface='{$interfacesArray[$i]}' AND method ='transmit' ORDER BY id DESC LIMIT 10");
             $timeStampArray = $this->db->query("SELECT timeStamp FROM `traffic` WHERE interface='{$interfacesArray[$i]}' AND method ='transmit' ORDER BY id DESC LIMIT 10");
             $downloadArray = $receiveArray->result_array();
             $uploadArray = $transmitArray->result_array();
             $timeArray = $timeStampArray->result_array();
-            
             foreach ($downloadArray as $value) {
                 foreach($value as $name => $receive){
                     $download[] = (int)$receive;
@@ -143,10 +180,9 @@ class Api extends CI_Controller {
         }
         return array($json);
         
-        
     }
     
-    public function serviceStatus(){
+    private function getServiceStatus(){
         $this->load->library('command');
         // 命令
         $getMemInfo = new Command('cat');
@@ -202,25 +238,26 @@ class Api extends CI_Controller {
         
     }
     
+    /************************ setting process function ********************************************/
     //服务重新启动
     // nohup /usr/local/bin/ss-redir -c $configfile_path -d start >/dev/null 2>&1&
     // nohup /usr/local/bin/ss-tunnel -c $configfile_path -l $ss_tunnel_port -u -L $ss_tunnel_address > /dev/null 2>&1&
     // process_name configfile_path 参数必须给出,ss-tunnel 地址和 port 可选
-    public function serviceRestart($process_name,$configfile_path,$ss_tunnel_address="",$ss_tunnel_port=""){
+    private function serviceRestart($process_name,$configfile_path,$ss_tunnel_address="",$ss_tunnel_port=""){
         $this->load->library('command');
         if( $process_name == "ss-tunnel" && isset($configfile_path)){
             $restartService = new Command('nohup /usr/local/bin/'+ $process_name + '-c' + $configfile_path + '-l' + $ss_tunnel_port + '-u -L' + $ss_tunnel_address + '> /dev/null 2>&1&');
             if ($restartService->execute()) {
                 echo $checkRedirStatus->getOutput();
                 //执行成功
-                return 0;
+                return 1;
             }
             else {
                 echo "ss-tunnel start failed  \n";
                 echo $restartService->getError();
                 $exitCode = $restartService->getExitCode();
                 //执行失败
-                return 1;
+                return 0;
             }
         }
         else if( $process_name == "ss-redir" && isset($configfile_path)){
@@ -228,66 +265,24 @@ class Api extends CI_Controller {
             if ($restartService->execute()) {
                 echo $checkRedirStatus->getOutput();
                 //执行成功
-                return 0;
+                return 1;
             }
             else {
                 echo "ss-redir start failed \n";
                 echo $restartService->getError();
                 $exitCode = $restartService->getExitCode();
                 //执行失败
-                return 1;
+                return 0;
             }
         }
         
     }
     
     
-    //处理 Get 请求参数
-    public function resolveGet(){
-        //接受所有参数并经过 XSS 过滤 params= TRUE
-        //返回参数+ip 关联数组
-        $getParams = $this->input->get(NULL, TRUE);
-        $ip=array('ip' => $this->input->ip_address());
-        $params=array_merge_recursive($getParams,$ip);
-        // print_r($params);
-        return $params;
-    }
-    
-    //处理 Post 请求参数
-    public function resolvePost(){
-        $postParams=$this->input->post(NULL, TRUE);
-        $ip=array('ip' => $this->input->ip_address());
-        $params=array_merge_recursive($postParams,$ip);
-        // print_r($params);
-        return $params;
-    }
-    
-    public function serviceReload(){
-        $params = $this->resolvePost();
-        if (isset($params['restartService'])) {
-            //TODO
-            $statusCode = $this->serviceRestart($params['restartService'],"");
-            $this->callback($statusCode,$params['restartService']);
-        }
-        else {
-            return 0;
-        }
-    }
     
     
-    //服务启动后回调
-    /* 返回失败格式
-    {
-    "restartService": "",
-    "status": null
-    }
-    返回成功格式
-    {
-    "restartService": "重启的服务名称",
-    "status": 0
-    }
-    */
-    public function callback($statusCode,$restartService){
+    //封装重启服务回调接口 json
+    private function callback($statusCode,$restartService){
         $response = array('restartService' => $restartService, 'status' => $statusCode);
         $this->output
         ->set_status_header(200)
@@ -298,54 +293,60 @@ class Api extends CI_Controller {
         exit;
     }
     
-    
-    
-    
+    /************************ json output function ********************************************/
     //封装域名接口 json
-    public function domain(){
+    private function domain(){
         $response = $this->domain_meta_query();
-        $this->output
-        ->set_status_header(200)
-        ->set_header('Access-Control-Allow-Origin: *')
-        ->set_content_type('application/json', 'utf-8')
-        ->set_output(json_encode($response, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES))
-        ->_display();
-        exit;
+        $this->jsonOutput($response);
     }
     
     //封装流量接口 json
-    public function traffic(){
+    private function traffic(){
         $response = $this->traffic_meta_query();
-        $this->output
-        ->set_status_header(200)
-        ->set_header('Access-Control-Allow-Origin: *')
-        ->set_content_type('application/json', 'utf-8')
-        ->set_output(json_encode($response, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES))
-        ->_display();
-        exit;
+        $this->jsonOutput($response);
     }
     
     //封装 ServiceStatus 接口 json
-    public function queryServiceStatus(){
-        $response = $this->serviceStatus();
-        $this->output
-        ->set_status_header(200)
-        ->set_header('Access-Control-Allow-Origin: *')
-        ->set_header('Cache-Control: no-store, no-cache, must-revalidate')
-        ->set_header('Pragma: no-cache')
-        ->set_content_type('application/json', 'utf-8')
-        ->set_output(json_encode($response, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES))
-        ->_display();
-        exit;
+    private function serviceStatus(){
+        $response = $this->getServiceStatus();
+        $this->jsonOutput($response);
     }
+    
+    /************************ API function ********************************************/
+    //query 接口
+    public function query()
+    {
+        $params = $this->resolveRequest();
+        if ($params['method'] == 'get'){
+            if ($params['query']== 'traffic') {
+                $this->traffic();
+            }
+            elseif ($params['query']== 'domain') {
+                $this->domain();
+            }
+            elseif ($params['query']== 'serviceStatus') {
+                $this->serviceStatus();
+            }
+        }
+    }
+    
+    //重启服务接口
+    public function serviceReload(){
+        $params = $this->resolveRequest();
+        if (isset($params['restartService'])) {
+            //TODO
+            $statusCode = $this->serviceRestart($params['restartService'],"");
+            $this->callback($statusCode,$params['restartService']);
+        }
+        else {
+            return 0;
+        }
+    }
+    
+    // 视图
     
     public function index(){
         $this->load->view('status');
-    }
-    
-    public function test()
-    {
-        $result=$this->serviceRestart('ss-tunnel','lalal');
     }
     
 }
